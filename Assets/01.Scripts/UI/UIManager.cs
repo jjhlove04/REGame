a3c4738854b87d282984e795d746e45fdefdaea2
jjhlove04 <jjhlovejh04@naver.com>
2022-02-11 13:14:52 +0900

    UI Add

--- Assets/01.Scripts/UI/UIManager.cs
+++ Assets/01.Scripts/UI/UIManager.cs
@@ -2,15 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager _ui = new UIManager();
     public static UIManager UI { get { return _ui; } }
+    [SerializeField] private GameObject upGradePanel;
+    public bool openPanel = false;
     public List<Button> panelOpenList = new List<Button>();
 
     private void Start() {
         
+    }
+
+    //패널 오픈 함수
+    public void UpGradePanelOpen()
+    {
+        if(openPanel == false)
+        {
+            upGradePanel.transform.DOMoveX(130, 1.2f);
+        }
+        if(openPanel)
+        {
+            upGradePanel.transform.DOMoveX(-130, 1.2f);
+
+        }
     }
 
 }
