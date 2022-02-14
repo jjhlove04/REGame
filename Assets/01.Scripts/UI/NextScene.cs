0c4f46e70f4934fde23944aca1fb03075233d151
jjhlove04 <jjhlovejh04@naver.com>
2022-02-14 14:57:43 +0900

    체력바, 타이틀 씬 작업

---
+++ Assets/01.Scripts/UI/NextScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NextScene : MonoBehaviour
+{
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button SettingButton;
+
+    private void Start() {
+        StartButton.onClick.AddListener(()=> SceneManager.LoadScene("Main"));     
+    }
+}
