30d8b12202ac2ed28d1a00cc776286113fc42b64
DESKTOP-77DPHAE\ROG <a>
2022-03-21 21:52:03 +0900

    많은 많은 많은 이펙트를 수정하고 나누고 힘들었다. 암튼 이것저것 많이 했다.

--- Assets/01.Scripts/ObjectPool/Spawner.cs
+++ Assets/01.Scripts/ObjectPool/Spawner.cs
@@ -15,16 +15,45 @@ public class Spawner : MonoBehaviour
     [SerializeField]
     private int round;
 
+    [SerializeField]
+    private bool boss = false;
+
     private void Start()
     {
         objectPool = FindObjectOfType<ObjectPool>();
 
-        SpawnMananger.Instance.spawn += new SpawnMananger.Spawn(SpwanEnemy);
+        if (boss)
+        {
+            SpawnMananger.Instance.spawn += new SpawnMananger.Spawn(SpwanEnemy);
+        }
+
+        else
+        {
+            SpawnMananger.Instance.spawn += new SpawnMananger.Spawn(BossSpwanEnemy);
+        }
+
     }
 
     private void SpwanEnemy(int s)
     {
         if(round <= SpawnMananger.Instance.round)
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                float randX = Random.Range(-interval, interval);
+                float randY = Random.Range(-interval, interval);
+
+                Vector3 randPos = new Vector3(randX,0,randY);
+
+                GameObject newPrefab = objectPool.GetObject(prefab);
+                newPrefab.transform.position = transform.position + randPos;
+            }
+        }
+    }
+    
+    private void BossSpwanEnemy(int s)
+    {
+        if(round == SpawnMananger.Instance.round)
         {
             for (int i = 0; i < spawnAmount; i++)
             {
