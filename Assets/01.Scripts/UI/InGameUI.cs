58ea0cd1f38a02858339ffa3f709933a4b0fc3ac
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-03-22 21:30:01 +0900

    UI 해상도 조절

--- Assets/01.Scripts/UI/InGameUI.cs
+++ Assets/01.Scripts/UI/InGameUI.cs
@@ -9,28 +9,45 @@ public class InGameUI : MonoBehaviour
     //UImanager로 다 옮겨야 함
     [SerializeField] private GameObject bluePrintTop;
     [SerializeField] private GameObject bluePrintBot;
+
     int index = 1;
     int num = 1;
     [SerializeField] private GameObject[] menuBtn;
+    [SerializeField] private RectTransform[] menuBtnRect ;
+    private List<RectTransform> menuBtnList = new List<RectTransform>();
     [SerializeField] private Button mainMenuBtn;
+    [SerializeField] private GameObject btnGroup;
+    int screenHeight = Screen.height;
+    int screenWidth = Screen.width;
 
-
+    RectTransform bpTop;
+    RectTransform bpBot;
+    private void Awake()
+    {
+        bpTop = bluePrintTop.GetComponent<RectTransform>();
+        bpBot = bluePrintBot.GetComponent<RectTransform>();
+        for(int i = 0; i < 4; i++)
+        {
+            menuBtnRect[i] = menuBtn[i].GetComponent<RectTransform>();
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        mainMenuBtn.onClick.AddListener(()=>
+
+        mainMenuBtn.onClick.AddListener(() =>
         {
             num *= -1;
 
             OpenMenu();
         });
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.T))
         {
             index = index * -1;
             OpenBluePrint(index);
@@ -39,41 +56,42 @@ public class InGameUI : MonoBehaviour
 
     public void OpenBluePrint(int index)
     {
-        if(index == -1)
+        if (index == -1)
         {
-            bluePrintBot.transform.DOMoveY(100,1.5f).SetEase(Ease.OutQuart);
-            bluePrintTop.transform.DOMoveY(410,1.5f).SetEase(Ease.OutQuart);
-        }
-        if(index == 1)
-        {
-            bluePrintTop.transform.DOMoveY(700,1.5f).SetEase(Ease.OutQuart);
-            bluePrintBot.transform.DOMoveY(-100,1.5f).SetEase(Ease.OutQuart);
+            bpTop.DOAnchorPosY(300, 1.5f).SetEase(Ease.OutQuart);
+            bpBot.DOAnchorPosY(-300, 1.5f).SetEase(Ease.OutQuart);
             
+
+        }
+        if (index == 1)
+        {
+
+            bpTop.DOAnchorPosY(781, 1.5f).SetEase(Ease.OutQuart);
+            bpBot.DOAnchorPosY(-789, 1.5f).SetEase(Ease.OutQuart);
         }
     }
-    
+
     public void OpenMenu()
     {
-        if(num == -1)
+        if (num == -1)
         {
-        menuBtn[0].SetActive(true);
-        menuBtn[1].SetActive(true);
-        menuBtn[2].SetActive(true);
-        menuBtn[3].SetActive(true);
-        menuBtn[0].transform.DOMoveY(470,1);
-        menuBtn[1].transform.DOMoveY(450,1);
-        menuBtn[2].transform.DOMoveY(430,1);
-        menuBtn[3].transform.DOMoveY(410,1);
+            btnGroup.SetActive(true);
+            menuBtnRect[0].DOAnchorPosY(-60,1f);
+            menuBtnRect[1].DOAnchorPosY(-100,1f);
+            menuBtnRect[2].DOAnchorPosY(-140,1f);
+            menuBtnRect[3].DOAnchorPosY(-180,1f);
         }
-        
-        if(num == 1)
+
+        if (num == 1)
         {
-            for(int i = 0; i < 4; i++)
+            for (int i = 0; i < 4; i++)
             {
-                menuBtn[i].transform.DOMoveY(500,1);
-                menuBtn[i].SetActive(false);
+                menuBtnRect[i].DOAnchorPosY(0,1).OnComplete(()=>
+                btnGroup.SetActive(false)
+                );
             }
+            
         }
-        
+
     }
 }
