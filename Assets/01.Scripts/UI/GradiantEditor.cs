d2b505ecd380bade1926d6b5edd0f326a7b9d551
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-04-05 19:19:57 +0900

    타이틀 수정, UI 에셋 추가

---
+++ Assets/01.Scripts/UI/GradiantEditor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GradiantEditor : MonoBehaviour
+{
+    public Gradient _gradiant;
+    public float _range;
+    private Text _text;
+
+    void Awake() 
+    {
+        _text = GetComponent<Text>();
+    }
+    void Update()
+    {
+        _text.color = _gradiant.Evaluate(_range);
+    }
+}
