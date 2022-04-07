ecfd57bfe3264537b244fcd6d2a334ec5e5a16f3
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-04-07 18:36:04 +0900

    UI만든거 올립니다 이전 수정사항 날라가도 전 이제 몰라요 수고요

---
+++ Assets/01.Scripts/UI/LoadingManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingManager : MonoBehaviour
+{
+    public static LoadingManager _instance;
+
+    private void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public async void LoadScene(string sceneName)
+    {
+        var scene = SceneManager.LoadSceneAsync(sceneName);
+    }
+}
