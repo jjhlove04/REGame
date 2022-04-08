797c0fca91f3fa379323ef502983785940c6200a
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-04-08 15:27:22 +0900

    로딩씬 스크립트 추가중

--- Assets/01.Scripts/UI/LoadingManager.cs
+++ Assets/01.Scripts/UI/LoadingManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LoadingManager : MonoBehaviour
@@ -8,6 +10,7 @@ public class LoadingManager : MonoBehaviour
     public static LoadingManager _instance;
 
     [SerializeField] private GameObject _loaderCanvas;
+    [SerializeField] private Image _progressBar;
     private void Awake()
     {
         if(_instance == null)
@@ -26,7 +29,19 @@ public class LoadingManager : MonoBehaviour
         var scene = SceneManager.LoadSceneAsync(sceneName);
         scene.allowSceneActivation = false;
 
-        
+        _loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+            _progressBar.fillAmount = scene.progress;
+        }
+        while(scene.progress < 0.8f);
+
+        await Task.Delay(1000);
+
+        scene.allowSceneActivation = true;
+        _loaderCanvas.SetActive(false);
 
     }
 }
