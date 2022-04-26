a59a8f9f5a459bc95a72b5cefed8bbc11334c2c4
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-04-26 10:18:23 +0900

    중간과정 커밋 + URP 추가

---
+++ Assets/01.Scripts/UI/SettingUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SettingUI : MonoBehaviour
+{
+    //option은 setting 안에서 움직이는 인덱스 Ex => settingIndex 1, 
+    //optionIndex 3
+    //지금 settingPanel 기준으로 resolution에 3번쨰 세팅
+
+    [SerializeField] private int _settingIndex;
+    [SerializeField] private int _optionIndex;
+    
+    Text _resolutionTxt;
+    Text _qualityTxt;
+
+    public void SetResolution()
+    {
+        switch(_optionIndex)
+        {
+            case 0: _resolutionTxt.text = string.Format("{0} * {1}",1920, 1080);
+            break;
+        }
+        
+    }
+
+}
