9eafd1170c8fbd7b7d0bea1b7f7abcedef12fd46
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-04-27 20:06:32 +0900

    메인 화면 설정 거의 완료 그래픽 설정 UI 완료 내일 기능 넣을 예정

--- Assets/01.Scripts/UI/SettingUI.cs
+++ Assets/01.Scripts/UI/SettingUI.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 using DG.Tweening;
 
 public class SettingUI : MonoBehaviour
 {
+
+    public RenderPipelineAsset[] qualityLevel;
+
     //option은 setting 안에서 움직이는 인덱스 Ex => settingIndex 1, 
     //optionIndex 3
     //지금 settingPanel 기준으로 resolution에 3번쨰 세팅
@@ -24,6 +28,11 @@ public class SettingUI : MonoBehaviour
             break;
         }
         
+    }
+    public  void ChangeQuality(int value)
+    {
+        QualitySettings.SetQualityLevel(value);
+        QualitySettings.renderPipeline = qualityLevel[value];
     }
 
 }
