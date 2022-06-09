5e9b3714cfa03ab31756f063b30ad5783f747b97
kimseong07 <kseonggyu1003@gmail.com>
2022-06-09 15:12:02 +0900

    포탑 돈나가기

--- Assets/01.Scripts/Manager/GameManager.cs
+++ Assets/01.Scripts/Manager/GameManager.cs
@@ -61,5 +61,10 @@ public class GameManager : MonoBehaviour
         { 
             state = State.End;
         }
+
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            goldAmount += 100;
+        }    
     }
 }
