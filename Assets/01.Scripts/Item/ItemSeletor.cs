1d9fb78a90bf3d3fc97680df385c833bcfffc4f1
seungbin1 <seungbin040622@gmail.com>
2022-06-16 14:06:04 +0900

    아이템 수량 업그레이드 만들었음

--- Assets/01.Scripts/Item/ItemSeletor.cs
+++ Assets/01.Scripts/Item/ItemSeletor.cs
@@ -17,27 +17,72 @@ public class ItemSeletor : MonoBehaviour
     bool count = false;
 
     [SerializeField]
-    int maxCount = 0;
+    private int maxCountPrice;
+
+    [SerializeField]
+    private int countPriceUp;
+
+    [SerializeField]
+    static int maxCount = 1;
+
+    [SerializeField]
+    private Button upgradeBtn;
+    [SerializeField]
+    private Text priceText;
 
     int curCount;
 
     [SerializeField]
     TextMeshProUGUI text;
-    
+
+    TestDatabase testDatabase;
+
 
     void Start()
     {
+        testDatabase = TestDatabase.Instance;
+
+        if (upgradeBtn != null)
+        {
+
+            TextPrice();
+
+            upgradeBtn.onClick.AddListener(() =>
+            {
+                UpgradeItmeCount();
+            });
+        }
+
         GetComponent<Button>().onClick.AddListener(() => 
         {
             ItemManager.Instance.SelecteItem(item, price, background, count, maxCount, out curCount);
 
             if(curCount != 0)
             {
-                text.gameObject.SetActive(true);
+                if(testDatabase.resultGold >= price)
+                {
+                    text.gameObject.SetActive(true);
 
-                text.text = "" + curCount + " / " + maxCount;
+                    text.text = "" + curCount + " / " + maxCount;
+                }
             }
         });
     }
 
+    public void UpgradeItmeCount()
+    {
+        if(testDatabase.resultGold >= maxCountPrice + countPriceUp * (maxCount - 1))
+        {
+            testDatabase.resultGold -= maxCountPrice + countPriceUp * (maxCount-1);
+
+            maxCount++;
+        }
+
+        TextPrice();
+    }
+
+    public void TextPrice()
+    {
+        priceText.text = ""+(maxCountPrice + countPriceUp * (maxCount - 1));
+    }
 }
