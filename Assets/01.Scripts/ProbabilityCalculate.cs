675199f555e522f7706984eb91e066f7b7089886
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-07-07 16:26:33 +0900

    확률계산기 에딧

---
+++ Assets/01.Scripts/ProbabilityCalculate.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProbabilityCalculate
+{
+    public static bool ReturnProbability(float percent)
+    {
+        //수가 너무 작을시 값 고정
+        if(percent < 0.1f)
+        {
+            percent = 0.1f;
+        }
+
+        bool result = false;
+        int arrCount = 100;
+        float randomHit = percent * arrCount;
+        int random = UnityEngine.Random.Range(1, arrCount + 1);
+        if(random <= randomHit)
+        {
+            result = true;
+        }
+        
+        return result;
+    }
+}
