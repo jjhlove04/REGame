1c4b9906416a1725b34e466fd11054ce77b5a53d
Jeong_Jin_Hyo <jjhlovejh04@naver.com>
2022-09-21 12:51:33 +0900

    titlemove ver2

--- Assets/01.Scripts/1-1.J/TitleInterfaceManager.cs
+++ Assets/01.Scripts/1-1.J/TitleInterfaceManager.cs
@@ -9,7 +9,9 @@ public class TitleInterfaceManager : MonoBehaviour
     public static TitleInterfaceManager Instance = new TitleInterfaceManager();
     
     public int sceneIndex = 0;
-    bool canEscape = false;
+    public bool canEscape = false;
+    //세부타이틀 들어갔다 나올때.
+    public bool canSubEscape = false;
     
     [Header("타이틀 - 버튼")]
     public RectTransform btnGroup;
@@ -33,11 +35,16 @@ public class TitleInterfaceManager : MonoBehaviour
     public GameObject[] canvasGroup;
 
     [Header("업그레이드 - 오브젝트")]
+    bool isNowTitel = false;
     public GameObject fadeImg;
     public GameObject[] upPanel;
     
 
     private void Awake() {
+        Sequence first = DOTween.Sequence();
+        first.Append(fadeImg.transform.DOScale(1,0.1f));
+        first.Append(fadeImg.transform.DOScale(0,0.1f));
+
         //최초 한번만 실행
         btnGroupAction(0);
 
@@ -55,7 +62,7 @@ public class TitleInterfaceManager : MonoBehaviour
             btnGroupAction(1);
             sceneIndex = 2;
             IntitleAction(0);
-            backBtn.gameObject.SetActive(true);         
+            backBtn.gameObject.SetActive(true);
 
         });
         titleBtn[2].onClick.AddListener(()=> {
@@ -64,11 +71,21 @@ public class TitleInterfaceManager : MonoBehaviour
 
         });
 
-        inTitleBtn[1].onClick.AddListener(()=>
-        {
-
+        inTitleBtn[1].onClick.AddListener(()=> {
+            IntitleAction(2);
+            isNowTitel = true;
+            sceneIndex = 3; 
         });
-
+        inTitleBtn[2].onClick.AddListener(()=> {
+            IntitleAction(4);
+            isNowTitel = true;
+            sceneIndex = 4; 
+        });
+        inTitleBtn[3].onClick.AddListener(()=> {
+            IntitleAction(6);
+            isNowTitel = true;
+            sceneIndex = 5; 
+        });
         backBtn.onClick.AddListener(()=>
         {
             EscapeBtn();
@@ -120,12 +137,17 @@ public class TitleInterfaceManager : MonoBehaviour
     {
         Sequence upBtnShow = DOTween.Sequence();
         Sequence upBtnClose = DOTween.Sequence();
-        Sequence upPanelShow = DOTween.Sequence();
+        Sequence upPanel1Show = DOTween.Sequence();
+        Sequence upPanel2Show = DOTween.Sequence();
+        Sequence upPanel3Show = DOTween.Sequence();
+        Sequence upPanel1Close = DOTween.Sequence();
+        Sequence upPanel2Close = DOTween.Sequence();
+        Sequence upPanel3Close = DOTween.Sequence();
         if(index == 0)
         {
-            upBtnShow.Append(inTitleBtn[1].GetComponent<Text>().DOFade(1,0.4f));
-            upBtnShow.Append(inTitleBtn[2].GetComponent<Text>().DOFade(1,0.4f));
-            upBtnShow.Append(inTitleBtn[3].GetComponent<Text>().DOFade(1,0.4f).OnComplete(()=>{
+            upBtnShow.Append(inTitleBtn[1].GetComponent<Text>().DOFade(1,0.3f));
+            upBtnShow.Append(inTitleBtn[2].GetComponent<Text>().DOFade(1,0.3f));
+            upBtnShow.Append(inTitleBtn[3].GetComponent<Text>().DOFade(1,0.3f).OnComplete(()=>{
                 canEscape = true;
             }));
             upBtnShow.SetAutoKill(false);
@@ -134,21 +156,140 @@ public class TitleInterfaceManager : MonoBehaviour
         {
             upBtnClose.Append(inTitleBtn[1].GetComponent<Text>().DOFade(0,0.2f));
             upBtnClose.Join(inTitleBtn[2].GetComponent<Text>().DOFade(0,0.2f));
-            upBtnClose.Join(inTitleBtn[3].GetComponent<Text>().DOFade(0,0.2f));
+            upBtnClose.Join(inTitleBtn[3].GetComponent<Text>().DOFade(0,0.2f).OnComplete(()=> 
+            {
+                    canvasGroup[1].SetActive(false);
+                
+            }));
             upBtnClose.SetAutoKill(false);
         }
-        if(index ==3)
+
+        //up기차 들어가기
+        if(index == 2)
         {
-            upPanelShow.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=>{
+            upPanel1Show.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel1Show.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=>{
+                IntitleAction(9);
                 upPanel[0].SetActive(true);
-                upPanelShow.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f));
+                upPanel1Show.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>{
+                    upPanel1Show.Append(fadeImg.transform.DOScale(0,0.1f).OnComplete(()=>
+                    {
+                        canEscape = false;
+                        canSubEscape = true;
+                    }));
+                }));
             }));
-            upPanelShow.SetAutoKill(false);
+            sceneIndex = 3;
+            upPanel1Show.SetAutoKill(false);
+        }
+        //up기차에서 나오기
+        if(index == 3)
+        {
+            upPanel1Close.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel1Close.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=> 
+            {
+                upPanel[0].SetActive(false);
+                upPanel1Close.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>
+                {
+                    upPanel1Close.Append(fadeImg.transform.DOScale(0,0.1f));
+                    IntitleAction(0);
+                }));
+                sceneIndex = 2;
+            }));
+            upPanel1Close.SetAutoKill(false);
+
+        }
+        //up 타워 패널 들어가기
+        if(index == 4)
+        {
+            upPanel2Show.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel2Show.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=>{
+                IntitleAction(9);
+                upPanel[1].SetActive(true);
+                upPanel2Show.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>{
+                    upPanel2Show.Append(fadeImg.transform.DOScale(0,0.1f).OnComplete(()=>
+                    {
+                        canEscape = false;
+                        canSubEscape = true;
+                    }));
+                }));
+            }));
+            sceneIndex = 4;
+            upPanel2Show.SetAutoKill(false);
+        }
+        //up타워에서 나오기
+        if(index == 5)
+        {
+            upPanel2Close.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel2Close.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=> 
+            {
+                upPanel[1].SetActive(false);
+                Debug.Log("패널 1 닫힘");
+
+                upPanel2Close.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>
+                {
+                    upPanel2Close.Append(fadeImg.transform.DOScale(0,0.1f));
+                    IntitleAction(0);
+                }));
+                sceneIndex = 2;
+            }));
+            upPanel2Close.SetAutoKill(false);
+
+        }
+
+        //up 아이템 뽑기
+        if(index == 6)
+        {
+            upPanel3Show.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel3Show.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=>{
+                IntitleAction(9);
+                upPanel[2].SetActive(true);
+                upPanel3Show.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>{
+                    upPanel3Show.Append(fadeImg.transform.DOScale(0,0.1f).OnComplete(()=>
+                    {
+                        canEscape = false;
+                        canSubEscape = true;
+                    }));
+                }));
+            }));
+            sceneIndex = 5;
+            upPanel3Show.SetAutoKill(false);
+        }
+        //up 아이템에서 나오기
+        if(index == 7)
+        {
+            upPanel3Close.Append(fadeImg.transform.DOScale(1,0.1f));
+            upPanel3Close.Append(fadeImg.GetComponent<Image>().DOFade(1,0.4f).OnComplete(()=> 
+            {
+                upPanel[2].SetActive(false);
+                Debug.Log("패널 2 닫힘");
+                upPanel3Close.Append(fadeImg.GetComponent<Image>().DOFade(0,0.4f).OnComplete(()=>
+                {
+                    upPanel3Close.Append(fadeImg.transform.DOScale(0,0.1f));
+                    IntitleAction(0);
+                }));
+                sceneIndex = 2;
+            }));
+            upPanel3Close.SetAutoKill(false);
+
+        }
+
+
+        //캔버스 살아있는 업버튼 트위닝 => 세부 패널 들어갔다가 나올때만 호출
+        if(index == 9)
+        {
+            upBtnClose.Append(inTitleBtn[1].GetComponent<Text>().DOFade(0,0.2f));
+            upBtnClose.Join(inTitleBtn[2].GetComponent<Text>().DOFade(0,0.2f));
+            upBtnClose.Join(inTitleBtn[3].GetComponent<Text>().DOFade(0,0.2f).OnComplete(()=> 
+            {
+            }));
+            upBtnClose.SetAutoKill(false);
         }
     }
 
     public void EscapeBtn()
     {
+        //출발 나오기
         if(sceneIndex == 1 && canEscape)
         {
             btnGroupAction(0);
@@ -156,12 +297,37 @@ public class TitleInterfaceManager : MonoBehaviour
             canEscape = false;
             backBtn.gameObject.SetActive(false);         
         }
+        //업그레이드 창 나가기
         if(sceneIndex == 2 && canEscape)
         {
             btnGroupAction(0);
-            canvasGroup[1].SetActive(false);
+            IntitleAction(1);
             canEscape = false;
-            backBtn.gameObject.SetActive(false);         
+            backBtn.gameObject.SetActive(false);
+            isNowTitel = false;      
+        }
+        //업그레이드 세부 창1 나가기 
+        if(sceneIndex == 3 && canSubEscape)
+        {   
+            canSubEscape = false;
+            IntitleAction(3);
+
+        }
+        //업그레이드 세부 창2 나가기 
+
+        if(sceneIndex == 4 && canSubEscape)
+        {   
+            canSubEscape = false;
+            IntitleAction(5);
+
+        }
+        //업그레이드 세부 창2 나가기 
+
+        if(sceneIndex == 5 && canSubEscape)
+        {   
+            canSubEscape = false;
+            IntitleAction(7);
+
         }
     }
 }
